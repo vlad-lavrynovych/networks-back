cfe489031496240a097493db3ad14584e101772d
Vlad <vlad300820001@gmail.com>
2020-02-20 15:35:27 +0200

    initial commit

---
+++ src/test/java/com/example/networks/NetworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.networks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
